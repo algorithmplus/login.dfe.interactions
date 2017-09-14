1be2f4e43845f39aec6b48957b34726c166ce9fe
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2017-09-14 08:26:40 +0100

    Update to the login screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const next = require('next');
+const fs = require('fs');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -8,7 +9,7 @@ const handle = app.getRequestHandler();
 
 app.prepare()
   .then(() => {
-    const server = express();
+    const app = express();
 
     const options = {
       key: (process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'dev') ? fs.readFileSync('./ssl/localhost.key') : null,
@@ -22,28 +23,29 @@ app.prepare()
       const server = https.createServer(options, app);
 
       server.listen(port, () => {
-        console.log(`Dev server listening on https://${process.env.HOST}:${process.env.PORT}`);
+        console.log(`Dev server listening on https://localhost:${process.env.PORT}`);
       });
     } else {
-      app.listen(port);
+      app.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+      });
     }
 
-    server.get('/cb', (req, res) => {
+    app.get('/cb', (req, res) => {
         res.set('Content-Type', 'text/html');
-        res.send(`<html><body><form id="cbform" action="https://localhost:4430/logincomplete" method="post"><input name="uuid" type="hidden" value=${req.query.uuid} /><input name="uid" type="hidden" value=${req.query.uid} /></form><script>document.getElementById('cbform').submit();</script></body>`);
+        res.send(`<html><body><form id="cbform" action="https://localhost:4430/interaction/${req.query.uuid}/complete" method="post"><input name="uuid" type="hidden" value=${req.query.uuid} /><input name="uid" type="hidden" value=${req.query.uid} /></form><script>document.getElementById('cbform').submit();</script></body>`);
     });
 
-    server.get('*', (req, res) => {
+    app.get('*', (req, res) => {
       return handle(req, res)
     });
 
-    server.post('/usernamepassword', (req, res) => {
+    app.post('/usernamepassword', (req, res) => {
        const uuid = req.query.uuid;
        res.redirect(`/cb?uuid=${uuid}&uid=abc`);
     });
 
-    server.listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
-    })
+
   });
+// https://localhost:4430/auth?client_id=foo&response_type=code&scope=openid+email&nonce=foobar&prompt=login&redirect_uri=https://localhost:1234/asdasd
