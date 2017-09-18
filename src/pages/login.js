cc88f2126edcc76e1c1411832538132bf4af6fee
James King <hello@jamesmichaelking.com>
2017-09-18 15:58:05 +0100

    Renamed Login page

---
+++ src/pages/login.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default class extends React.Component {
+    static getInitialProps({req}) {
+        const message = req ? req.query.message : '';
+        return { message }
+    }
+
+    render() {
+        return (
+            <Layout>
+                <div>{this.props.message}</div>
+                <form method="post">
+                    <div>
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username"/>
+                    </div>
+                    <div>
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" />
+                    </div>
+                    <button type="submit">Sign In</button>
+
+                </form>
+            </Layout>
+    )
+    }
+}
