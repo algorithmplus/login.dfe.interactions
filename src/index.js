126ede74e0734d252f1b90f2cb4bef4c8b53159e
Floyd Price <f.price@floydprice.com>
2017-09-20 15:23:58 +0100

    updated the views to use the new layout file, also included the dfe.toolkit css files from the cdn

--- src/index.js
+++ src/index.js
@@ -2,23 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 const routes = require('./Routes');
 const config = require('./Config');
 
 const app = express();
 
 // Add middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set view engine
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
 
+// Setup express layouts
+app.use(expressLayouts);
+app.set('layout extractScripts', true);
+app.set('layout', 'layouts/layout');
+
 // Setup routes
 routes.register(app);
 
 // Setup server
-if (config.hostingEnvironment.env == 'dev') {
+if (config.hostingEnvironment.env === 'dev') {
   app.proxy = true;
 
   const https = require('https');
@@ -26,15 +32,15 @@ if (config.hostingEnvironment.env == 'dev') {
     key: fs.readFileSync('./ssl/localhost.key'),
     cert: fs.readFileSync('./ssl/localhost.cert'),
     requestCert: false,
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
   };
   const server = https.createServer(options, app);
 
-  server.listen(config.hostingEnvironment.port, function () {
+  server.listen(config.hostingEnvironment.port, () => {
     console.log(`Dev server listening on https://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`);
-  })
+  });
 } else {
-  app.listen(config.hostingEnvironment.port, function() {
+  app.listen(config.hostingEnvironment.port, () => {
     console.log(`Dev server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`);
   });
 }
