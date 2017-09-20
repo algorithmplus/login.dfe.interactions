470ca7025359a940ec5a4a720145f49a562dac1f
Simon Bull <simonbu11@users.noreply.github.com>
2017-09-20 11:11:30 +0100

    Implement hardcoded user service

--- src/Users/UserService.js
+++ src/Users/UserService.js
@@ -1,6 +1,12 @@
 class UserService {
   authenticate(username, password){
+    if(username.toLowerCase() != 'foo@example.com' || !password || password.length < 1) {
+      return null;
+    }
 
+    return {
+      uid: '23121d3c-84df-44ac-b458-3d63a9a05497'
+    }
   }
 }
 
