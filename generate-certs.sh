510e90c0efc536c6de55bb4bf2449b8809823389
Floyd Price <f.price@floydprice.com>
2017-09-21 11:11:34 +0100

    updated generate-ssl to not overrite if exists

--- generate-certs.sh
+++ generate-certs.sh
@@ -3,8 +3,15 @@
 clear
 echo "Creating SSL certs for localhost."
 
-mkdir ssl
+if [ ! -d ssl ]; then
+  mkdir -p ssl;
 
-openssl genrsa -out ssl/localhost.key 2048
+    if [ ! -f ssl/localhost.txt ]; then
+        openssl genrsa -out ssl/localhost.key 2048
 
-openssl req -new -x509 -key ssl/localhost.key -out ssl/localhost.cert -days 3650 -subj /CN=localhost
+        openssl req -new -x509 -key ssl/localhost.key -out ssl/localhost.cert -days 3650 -subj /CN=localhost
+    fi
+
+fi
+
+
