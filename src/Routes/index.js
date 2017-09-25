2815be485630c7b2538e43d7aa78b9b24cc565d6
Simon Bull <simonbu11@users.noreply.github.com>
2017-09-25 11:40:53 +0100

    Add csrf protection to usernamepassword

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,8 +4,8 @@ const DevRoutes = require('./DevRoutes');
 const UsernamePasswordRoutes = require('./../UsernamePassword/UsernamePasswordRoutes');
 
 class Routes {
-  static register(app) {
-    UsernamePasswordRoutes.register(app);
+  static register(app, csrf) {
+    UsernamePasswordRoutes.register(app, csrf);
 
     if(Config.hostingEnvironment.env == 'dev') {
       DevRoutes.register(app);
