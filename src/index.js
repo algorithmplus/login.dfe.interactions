05ebc252afd8fc480723ae2b00f51b9ca8efb21d
Floyd Price <f.price@floydprice.com>
2017-09-25 16:39:17 +0100

    include morgan for express logging and winston for generic logging

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const fs = require('fs');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
-const csurf = require('csurf')
-const routes = require('./Routes');
+const csurf = require('csurf');
 const config = require('./Config');
+const morgan = require('morgan');
+const logger = require('winston');
 
 const usernamePassword = require('./UsernamePassword');
 const devLauncher = require('./DevLauncher');
@@ -14,9 +15,12 @@ const devLauncher = require('./DevLauncher');
 const app = express();
 const csrf = csurf({ cookie: true });
 
+
 // Add middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan('combined', { stream: fs.createWriteStream('./access.log', { flags: 'a' }) }));
+app.use(morgan('dev'));
 
 // Set view engine
 app.set('view engine', 'ejs');
@@ -44,10 +48,10 @@ if (config.hostingEnvironment.env === 'dev') {
   const server = https.createServer(options, app);
 
   server.listen(config.hostingEnvironment.port, () => {
-    console.log(`Dev server listening on https://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`);
+    logger.info(`Dev server listening on https://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`);
   });
 } else {
   app.listen(config.hostingEnvironment.port, () => {
-    console.log(`Dev server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`);
+    logger.info(`Dev server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`);
   });
 }
