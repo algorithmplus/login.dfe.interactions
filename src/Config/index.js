3d04fb5cace230fb73099f00aa78b8ddb49b98d0
Floyd Price <f.price@floydprice.com>
2017-09-25 17:20:55 +0100

    refactor config object

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,35 +1,40 @@
 const userService = require('./../Users/UserService');
 const fs = require('fs');
 
-module.exports = {
-  loggerSettings: {
-    levels: {
-      info: 0,
-      ok: 1,
-      error: 2
+const createConfig = (env) => {
+  const isDev = (env || 'dev') === 'dev';
+  return {
+    loggerSettings: {
+      levels: {
+        info: 0,
+        ok: 1,
+        error: 2,
+      },
+      colors: {
+        info: 'red',
+        ok: 'green',
+        error: 'yellow',
+      },
     },
-    colors: {
-      info: 'red',
-      ok: 'green',
-      error: 'yellow'
-    }
-  },
-  hostingEnvironment: {
-    env: process.env.NODE_ENV ? process.env.NODE_ENV : 'dev',
-    host: process.env.HOST ? process.env.HOST : 'localhost',
-    port: process.env.PORT ? process.env.PORT : 4431,
-    protocol: (process.env.NODE_ENV ? process.env.NODE_ENV : 'dev') == 'dev' ? 'https' : 'http'
-  },
-  oidcService: {
-    url: process.env.OIDC_BASE_URL ? process.env.OIDC_BASE_URL : 'https://localhost:4430'
-  },
-  crypto: {
-    signing: {
-      publicKey: fs.readFileSync('./ssl/localhost.cert', 'utf8'),
-      privateKey: fs.readFileSync('./ssl/localhost.key', 'utf8')
-    }
-  },
-  services: {
-    user: new userService()
-  }
+    hostingEnvironment: {
+      env: env || 'dev',
+      host: process.env.HOST ? process.env.HOST : 'localhost',
+      port: process.env.PORT ? process.env.PORT : 4431,
+      protocol: isDev ? 'https' : 'http',
+    },
+    oidcService: {
+      url: process.env.OIDC_BASE_URL ? process.env.OIDC_BASE_URL : 'https://localhost:4430',
+    },
+    crypto: {
+      signing: {
+        publicKey: isDev ? fs.readFileSync('./ssl/localhost.cert', 'utf8') : '',
+        privateKey: isDev ? fs.readFileSync('./ssl/localhost.key', 'utf8') : '',
+      },
+    },
+    services: {
+      user: new userService(),
+    },
+  };
 };
+
+module.exports = createConfig(process.env.NODE_ENV);
