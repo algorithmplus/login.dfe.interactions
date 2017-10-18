22aad96c87733d0aff89f7aee0f720971a0e3fa7
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-10-18 14:53:40 +0100

    Add userCodesAdapter
    Added usercodes adapter for generating, validating and deleting usercodes. These are used as part of the forgotten password flow

--- src/UserCodes/StaticUserCodeAdapter.js
+++ src/UserCodes/StaticUserCodeAdapter.js
@@ -1,17 +1,20 @@
 class StaticUserCodeAdapter {
-  upsertCode(username) {
-    return { sub: username, code: 'ABC123' };
+  upsertCode(userId) {
+    return { sub: userId, code: 'ABC123' };
   }
 
-  deleteCode(username) {
+  deleteCode(userId) {
     return true;
   }
 
-  validateCode(username, code) {
-    if(code === 'ABC123') {
-      return true;
+  validateCode(userId, code) {
+    if (code === 'ABC123') {
+      return {
+        uid: '23121d3c-84df-44ac-b458-3d63a9a05497',
+        code: 'ABC123',
+      };
     }
-    return false;
+    return null;
   }
 }
 
---
+++ src/UserCodes/UserCodesApiAdapter.js
@@ -0,0 +1,84 @@
+const rp = require('request-promise');
+const jwtStrategy = require('login.dfe.jwt-strategies');
+const config = require('./../Config');
+
+class UserCodesApiAdapter {
+  async upsertCode(userId) {
+    const token = await jwtStrategy(config.directories.service).getBearerToken();
+
+    try {
+      const user = await rp({
+        method: 'PUT',
+        uri: `${config.directories.service.url}/userCodes/upsert`,
+        headers: {
+          authorization: `bearer ${token}`,
+        },
+        body: {
+          uid: userId,
+        },
+        json: true,
+      });
+
+      return {
+        user,
+      };
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  async deleteCode(userId) {
+    const token = await jwtStrategy(config.directories.service).getBearerToken();
+
+    try {
+      const user = await rp({
+        method: 'DELETE',
+        uri: `${config.directories.service.url}/userCodes/${userId}`,
+        headers: {
+          authorization: `bearer ${token}`,
+        },
+        json: true,
+      });
+
+      return {
+        user,
+      };
+    } catch (e) {
+      const status = e.statusCode ? e.statusCode : 500;
+      if (status === 404) {
+        return null;
+      }
+      throw new Error(e);
+    }
+  }
+
+  async validateCode(userId, code) {
+    const token = await jwtStrategy(config.directories.service).getBearerToken();
+
+    try {
+      const userCode = await rp({
+        method: 'GET',
+        uri: `${config.directories.service.url}/userCodes/validate/${userId}/${code}`,
+        headers: {
+          authorization: `bearer ${token}`,
+        },
+        body: {
+          uid: userId,
+        },
+        json: true,
+      });
+
+      return {
+        userCode,
+      };
+    } catch (e) {
+      const status = e.statusCode ? e.statusCode : 500;
+      if (status === 404) {
+        return null;
+      }
+      throw new Error(e);
+    }
+  }
+}
+
+module.exports = UserCodesApiAdapter;
