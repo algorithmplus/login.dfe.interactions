79b471c1732daeaad04833c734d5174ebe3828b3
Simon Bull <simon.bull.84@googlemail.com>
2017-12-08 14:41:40 +0000

    Add tests for validate digipass postback

---
+++ test/Digipass/validateDigipassCode.test.js
@@ -0,0 +1,174 @@
+jest.mock('./../../src/infrastructure/Users', () => {
+  return {
+    getDevices: jest.fn(),
+  };
+});
+jest.mock('./../../src/infrastructure/devices', () => {
+  return {
+    validateDigipassToken: jest.fn(),
+  };
+});
+jest.mock('./../../src/infrastructure/logger');
+jest.mock('./../../src/app/InteractionComplete');
+
+const validateDigipassCode = require('./../../src/app/Digipass/validateDigipassCode');
+const utils = require('./../utils');
+
+describe('when validating the user entered digipass code', () => {
+  let req;
+  let res;
+  let getDevices;
+  let validateDigipassToken;
+  let processComplete;
+  let logger;
+
+  beforeEach(() => {
+    req = utils.mockRequest();
+    req.body.code = '12345678';
+    req.query.uid = 'user-1';
+
+    res = utils.mockResponse();
+
+    getDevices = require('./../../src/infrastructure/Users').getDevices;
+    getDevices.mockReset();
+    getDevices.mockReturnValue([
+      { id: '1', type: 'authenticator', serialNumber: '0987654' },
+      { id: '2', type: 'digipass', serialNumber: '567890' },
+    ]);
+
+    validateDigipassToken = require('./../../src/infrastructure/devices').validateDigipassToken;
+    validateDigipassToken.mockReset();
+    validateDigipassToken.mockReturnValue(true);
+
+    processComplete = require('./../../src/app/InteractionComplete').process;
+    processComplete.mockReset();
+
+    logger = require('./../../src/infrastructure/logger');
+  });
+
+  it('then it should get list of devices for user', async () => {
+    await validateDigipassCode(req, res);
+
+    expect(getDevices.mock.calls).toHaveLength(1);
+    expect(getDevices.mock.calls[0][0]).toBe('user-1');
+  });
+
+  it('then it should use digipass device to validate code', async () => {
+    await validateDigipassCode(req, res);
+
+    expect(validateDigipassToken.mock.calls).toHaveLength(1);
+    expect(validateDigipassToken.mock.calls[0][0]).toBe('567890');
+    expect(validateDigipassToken.mock.calls[0][1]).toBe('12345678');
+  });
+
+  it('then it should complete the interaction', async () => {
+    await validateDigipassCode(req, res);
+
+    expect(processComplete.mock.calls).toHaveLength(1);
+    expect(processComplete.mock.calls[0][0]).toBe(req.params.uuid);
+    expect(processComplete.mock.calls[0][1]).toMatchObject({
+      status: 'success',
+    });
+  });
+
+  it('then it should return errored view if code not entered', async () => {
+    req.body.code = undefined;
+
+    await validateDigipassCode(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][0]).toBe('Digipass/views/token');
+    expect(res.render.mock.calls[0][1]).toMatchObject({
+      csrfToken: 'token',
+      code: '',
+      validationMessages: {
+        code: 'You must enter your code',
+      },
+    });
+  });
+
+  it('then it should return errored view if code is not 8 characters long', async () => {
+    req.body.code = '123456';
+
+    await validateDigipassCode(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][0]).toBe('Digipass/views/token');
+    expect(res.render.mock.calls[0][1]).toMatchObject({
+      csrfToken: 'token',
+      code: '',
+      validationMessages: {
+        code: 'Your code must be 8 digits',
+      },
+    });
+  });
+
+  it('then it should return errored view if code is not numeric', async () => {
+    req.body.code = 'abcdefgh';
+
+    await validateDigipassCode(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][0]).toBe('Digipass/views/token');
+    expect(res.render.mock.calls[0][1]).toMatchObject({
+      csrfToken: 'token',
+      code: '',
+      validationMessages: {
+        code: 'Your code must be 8 digits',
+      },
+    });
+  });
+
+  it('then it should return errored view if user has no devices registered', async () => {
+    getDevices.mockReturnValue([]);
+
+    await validateDigipassCode(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][0]).toBe('Digipass/views/token');
+    expect(res.render.mock.calls[0][1]).toMatchObject({
+      csrfToken: 'token',
+      code: '',
+      validationMessages: {
+        code: 'The code you entered is invalid',
+      },
+    });
+    expect(validateDigipassToken.mock.calls).toHaveLength(0);
+  });
+
+  it('then it should return errored view if user has no digipass devices registered', async () => {
+    getDevices.mockReturnValue([
+      { id: '1', type: 'authenticator', serialNumber: '0987654' },
+    ]);
+
+    await validateDigipassCode(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][0]).toBe('Digipass/views/token');
+    expect(res.render.mock.calls[0][1]).toMatchObject({
+      csrfToken: 'token',
+      code: '',
+      validationMessages: {
+        code: 'The code you entered is invalid',
+      },
+    });
+    expect(validateDigipassToken.mock.calls).toHaveLength(0);
+  });
+
+  it('then it should return errored view if code not valid', async () => {
+    validateDigipassToken.mockReturnValue(false);
+
+    await validateDigipassCode(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][0]).toBe('Digipass/views/token');
+    expect(res.render.mock.calls[0][1]).toMatchObject({
+      csrfToken: 'token',
+      code: '',
+      validationMessages: {
+        code: 'The code you entered is invalid',
+      },
+    });
+    expect(validateDigipassToken.mock.calls).toHaveLength(1);
+  });
+});
