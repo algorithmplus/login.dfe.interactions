cd21d44766e0c0e2facd764173a346f90432ae00
Floyd Price <f.price@floydprice.com>
2018-02-12 16:34:22 +0000

    Add tls support for Redis RateLimiter

--- src/app/rateLimit/index.js
+++ src/app/rateLimit/index.js
@@ -9,8 +9,10 @@ let store;
 if (config.hostingEnvironment.env === 'dev1') {
   store = new ExpressBrute.MemoryStore();
 } else {
+  const tls = config.hostingEnvironment.rateLimitUrl.includes('6380');
   const client = Redis.createClient({
     url: config.hostingEnvironment.rateLimitUrl,
+    tls,
   });
   store = new RedisStore({
     client,
