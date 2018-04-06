f356074521528cca7c4dc8f95b7f79aba4b7d750
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-04-06 08:27:57 +0100

    Add OSA api adapter and static adapter

---
+++ src/infrastructure/osa/OsaApiAdapter.js
@@ -0,0 +1,45 @@
+const config = require('./../Config')();
+const KeepAliveAgent = require('agentkeepalive').HttpsAgent;
+const rp = require('request-promise').defaults({
+  agent: new KeepAliveAgent({
+    maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+    maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+    timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+    keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
+  }),
+});
+const jwtStrategy = require('login.dfe.jwt-strategies');
+
+const authenticate = async (username, password, correlationId) => {
+  const token = await jwtStrategy(config.osaApi.service).getBearerToken();
+  try {
+    const user = await rp({
+      method: 'POST',
+      uri: `${config.osaApi.service.url}/authenticate`,
+      headers: {
+        authorization: `bearer ${token}`,
+        'x-correlation-id': correlationId,
+      },
+      body: {
+        username,
+        password,
+      },
+      json: true,
+    });
+
+    return {
+      user,
+    };
+  } catch (e) {
+    const status = e.statusCode ? e.statusCode : 500;
+    if (status === 404 || status === 403) {
+      return null;
+    }
+    throw new Error(e);
+  }
+};
+
+
+module.exports = {
+  authenticate,
+};
---
+++ test/UsernamePasswordTests/OsaApiAdapter.authenticate.test.js
@@ -0,0 +1,84 @@
+jest.mock('request-promise');
+jest.mock('agentkeepalive', () => ({
+  HttpsAgent: jest.fn(),
+}));
+jest.mock('login.dfe.jwt-strategies');
+jest.mock('../../src/infrastructure/Config');
+
+const rp = jest.fn();
+const requestPromise = require('request-promise');
+
+requestPromise.defaults.mockReturnValue(rp);
+
+
+describe('When authenticating a user with the OSA api', () => {
+  const username = 'user.one@unit.tests';
+  const password = 'mary-had-a-little-lamb';
+  const bearerToken = 'some-token';
+
+  let jwtGetBearerToken;
+
+  let osaApiUserAdapter;
+
+  beforeEach(() => {
+    rp.mockReset().mockReturnValue('user1');
+
+    jwtGetBearerToken = jest.fn().mockReturnValue(bearerToken);
+    const jwt = require('login.dfe.jwt-strategies');
+    jwt.mockImplementation(jwtConfig => ({
+      getBearerToken: jwtGetBearerToken,
+    }));
+
+    const config = require('./../../src/infrastructure/Config');
+    config.mockImplementation(() => ({
+      osaApi: {
+        service: {
+          url: 'https://osa.login.dfe.test',
+        },
+      },
+      hostingEnvironment: {
+        agentKeepAlive: {},
+      },
+    }));
+
+    osaApiUserAdapter = require('./../../src/infrastructure/osa/OsaApiAdapter');
+  });
+
+  it('it should post to the osa api', async () => {
+    await osaApiUserAdapter.authenticate(username, password);
+
+    expect(rp.mock.calls[0][0].method).toBe('POST');
+    expect(rp.mock.calls[0][0].uri).toBe('https://osa.login.dfe.test/authenticate');
+  });
+
+  it('it should send entered username and password', async () => {
+    await osaApiUserAdapter.authenticate(username, password);
+
+    expect(rp.mock.calls[0][0].body.username).toBe(username);
+    expect(rp.mock.calls[0][0].body.password).toBe(password);
+  });
+
+  it('it should user the jwt token for authorization', async () => {
+    await osaApiUserAdapter.authenticate(username, password);
+
+    expect(rp.mock.calls[0][0].headers.authorization).toBe(`bearer ${bearerToken}`);
+  });
+
+  it('then it should throw an error if the api call failed', async () => {
+    rp.mockImplementation(() => {
+      const error = new Error();
+      error.statusCode = 500;
+      throw error;
+    });
+
+    try {
+      await osaApiUserAdapter.authenticate(username, password);
+      throw new Error('No error thrown!');
+    } catch (e) {
+      if (e.message === 'No error thrown!') {
+        throw e;
+      }
+    }
+  });
+
+});
