644e7c5a715b3843ad49888e0a4e0b3ef8e013c8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-04-10 10:45:24 +0100

    Add route and action for change email

--- src/app/migration/index.js
+++ src/app/migration/index.js
@@ -6,6 +6,7 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const get = require('./getMigrationIntro');
 const migratedUserDetails = require('./getMigratedUserDetails');
+const migratedEmail = require('./getMigratedEmail');
 
 const router = express.Router({ mergeParams: true });
 
@@ -13,7 +14,7 @@ const registerRoutes = (csrf) => {
   logger.info('Mounting Migration routes');
   router.get('/', csrf, asyncWrapper(get));
   router.get('/userDetails', csrf, asyncWrapper(migratedUserDetails));
-
+  router.get('/email', csrf, asyncWrapper(migratedEmail))
   return router;
 };
 
