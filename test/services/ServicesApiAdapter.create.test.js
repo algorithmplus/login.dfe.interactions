2e502c20f72879a24ce53b1944a3185729380a9e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-04-18 15:10:49 +0100

    Add services api endpoint for creating user to organisation and serivce reference

---
+++ test/services/ServicesApiAdapter.create.test.js
@@ -0,0 +1,89 @@
+jest.mock('request-promise');
+jest.mock('login.dfe.jwt-strategies');
+jest.mock('../../src/infrastructure/Config');
+jest.mock('agentkeepalive', () => ({
+  HttpsAgent: jest.fn(),
+}));
+const rp = jest.fn();
+const requestPromise = require('request-promise');
+
+requestPromise.defaults.mockReturnValue(rp);
+
+describe('When creating a user with the api', () => {
+  const userId = 'user-1';
+  const serviceId = 'svc-1';
+  const organisationId = 'org-1';
+  const orgType = '001';
+  const bearerToken = 'some-token';
+  const expectedCorrelationId = 'some-uid';
+
+  let jwtGetBearerToken;
+
+  let servicesApiUserAdapter;
+
+  beforeEach(() => {
+    rp.mockReset().mockReturnValue('user1');
+
+    jwtGetBearerToken = jest.fn().mockReturnValue(bearerToken);
+    const jwt = require('login.dfe.jwt-strategies');
+    jwt.mockImplementation(jwtConfig => ({
+      getBearerToken: jwtGetBearerToken,
+    }));
+
+    const config = require('./../../src/infrastructure/Config');
+    config.mockImplementation(() => ({
+      organisations: {
+        service: {
+          url: 'https://organisations.login.dfe.test',
+        },
+      },
+      hostingEnvironment: {
+        agentKeepAlive: {},
+      },
+    }));
+
+    servicesApiUserAdapter = require('./../../src/infrastructure/Services/ServicesApiAdapter');
+  });
+
+  it('then the data is posted to the organisations api', async () => {
+    await servicesApiUserAdapter.create(userId, serviceId, organisationId, orgType, expectedCorrelationId);
+
+    expect(rp.mock.calls[0][0].method).toBe('POST');
+    expect(rp.mock.calls[0][0].uri).toBe(`https://organisations.login.dfe.test/organisations/${organisationId}/services/${serviceId}/create/${userId}`);
+  });
+
+  it('then the bearer token is passed in the header', async () => {
+    await servicesApiUserAdapter.create(userId, serviceId, organisationId, orgType, expectedCorrelationId);
+
+    expect(rp.mock.calls[0][0].headers.authorization).toBe(`bearer ${bearerToken}`);
+  });
+
+  it('then the correlation id is passed in the header', async () => {
+    await servicesApiUserAdapter.create(userId, serviceId, organisationId, orgType, expectedCorrelationId);
+
+    expect(rp.mock.calls[0][0].headers['x-correlation-id']).toBe(expectedCorrelationId);
+  });
+
+  it('then the body parameters are passed to the API', async () => {
+    await servicesApiUserAdapter.create(userId, serviceId, organisationId, orgType, expectedCorrelationId);
+
+    expect(rp.mock.calls[0][0].body.org_type).toBe(orgType);
+  });
+
+  it('then an error is thrown if the api call failed', async () => {
+    rp.mockImplementation(() => {
+      const error = new Error();
+      error.statusCode = 500;
+      throw error;
+    });
+
+    try {
+      await servicesApiUserAdapter.create(userId, serviceId, organisationId, orgType, expectedCorrelationId);
+      throw new Error('No error thrown!');
+    } catch (e) {
+      if (e.message === 'No error thrown!') {
+        throw e;
+      }
+    }
+  });
+});
