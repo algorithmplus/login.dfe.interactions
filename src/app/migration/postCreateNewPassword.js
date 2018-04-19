763ea55a9e4eff10a052a2a884ca81cff8ed80e2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-04-19 09:53:03 +0100

    Add create new password for migration user

---
+++ src/app/migration/postCreateNewPassword.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const users = require('./../../infrastructure/Users');
+const services = require('./../../infrastructure/Services');
+const userCodes = require('./../../infrastructure/UserCodes');
+const logger = require('./../../infrastructure/logger');
+const { validate } = require('./../utils/validatePassword');
+
+const action = async (req, res) => {
+  const validationResult = validate(req.body.newPassword, req.body.confirmPassword);
+
+  if (validationResult.failed) {
+    res.render('migration/views/createPassword', {
+      csrfToken: req.csrfToken(),
+      newPassword: '',
+      confirmPassword: '',
+      validationFailed: true,
+      validationMessages: validationResult.messages,
+    });
+    return;
+  }
+
+  const userCode = await userCodes.getCode(req.body.emailConfId, req.id, 'ConfirmMigratedEmail');
+
+  if (!userCode || !userCode.userCode || !userCode.userCode.contextData) {
+    res.render('migration/views/createPassword', {
+      csrfToken: req.csrfToken(),
+      newPassword: '',
+      confirmPassword: '',
+      validationFailed: true,
+      validationMessages: {
+        general: 'An error has occurred.',
+      },
+    });
+    return;
+  }
+
+  const userToMigrate = JSON.parse(userCode.userCode.contextData);
+
+  const user = await users.create(userCode.userCode.email, req.body.newPassword, userToMigrate.firstName, userToMigrate.lastName, req.id);
+
+  await userCodes.deleteCode(req.body.emailConfId, req.id, 'ConfirmMigratedEmail');
+
+  if (!user) {
+    res.render('migration/views/createPassword', {
+      csrfToken: req.csrfToken(),
+      newPassword: '',
+      confirmPassword: '',
+      validationFailed: true,
+      validationMessages: {
+        general: 'Email address has already been registered. Please sign in using your email address',
+      },
+    });
+    return;
+  }
+
+  let orgId;
+  if(userToMigrate.organisation.type === '001') {
+    orgId = userToMigrate.organisation.urn;
+  } else {
+    orgId = userToMigrate.organisation.uid;
+  }
+
+  services.create(user.id, userToMigrate.serviceId, orgId, userToMigrate.organisation.type, req.id);
+
+  req.session.migrationUser = undefined;
+
+  req.session.redirectUri = userCode.userCode.redirectUri;
+
+  res.redirect(`/${req.params.uuid}/migration/complete`);
+};
+
+module.exports = action;
---
+++ test/migrationTests/postCreateNewPassword.test.js
@@ -0,0 +1,144 @@
+jest.mock('./../../src/infrastructure/UserCodes');
+jest.mock('./../../src/infrastructure/Users');
+jest.mock('./../../src/infrastructure/Services');
+jest.mock('./../../src/infrastructure/Config', () => jest.fn().mockImplementation(() => ({
+  hostingEnvironment: {
+    agentKeepAlive: {},
+  },
+})));
+jest.mock('./../../src/infrastructure/logger', () => ({
+  info: jest.fn(),
+  audit: jest.fn(),
+}));
+const utils = require('./../utils');
+const postCreateNewPassword = require('./../../src/app/migration/postCreateNewPassword');
+
+describe('When posting to create a user from migration', () => {
+  let req;
+  let res;
+  let userCodesGetCode;
+  let userCodesDeleteCode;
+  let createUser;
+  let createOrg;
+  const expectedUserCodeId = 'some-uid';
+  const expectedPassword = 'my-super-strong-password-for-test';
+  const expectedEmail = 'test@local';
+  const expectedUserId = 'user-1';
+
+  beforeEach(() => {
+    req = utils.mockRequest();
+    res = utils.mockResponse();
+
+    req.body = {
+      newPassword: expectedPassword,
+      confirmPassword: expectedPassword,
+      emailConfId: expectedUserCodeId,
+    };
+
+    userCodesDeleteCode = jest.fn();
+    userCodesGetCode = jest.fn().mockReset().mockReturnValue({ userCode: { email: expectedEmail, code: '', contextData: '{"firstName":"Roger","lastName":"Johnson","email":"foo3@example.com","organisation":{"id":"72711ff9-2da1-4135-8a20-3de1fea31073","name":"Some School - MAT","urn":null,"localAuthority":null,"type":"010","uid":"MAT1234"},"clientName":"Some very friendly client","clientId":"profiles","redirectUri":"https://localhost:4431","serviceId":"svc1"}', redirectUri: 'https://localhost:4431' } });
+    const userCodes = require('./../../src/infrastructure/UserCodes');
+    userCodes.getCode = userCodesGetCode;
+    userCodes.deleteCode = userCodesDeleteCode;
+
+    createUser = jest.fn().mockReset().mockReturnValue({ id: expectedUserId });
+    const users = require('./../../src/infrastructure/Users');
+    users.create = createUser;
+
+    createOrg = jest.fn().mockReset();
+    const services = require('./../../src/infrastructure/Services');
+    services.create = createOrg;
+  });
+
+  it('then if the password is empty an error is returned', async () => {
+    req.body.newPassword = '';
+
+    await postCreateNewPassword(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][1].validationFailed).toBe(true);
+  });
+
+  it('then if the confirm password does not match the password an error is returned', async () => {
+    req.body.newPassword = 'my-super-strong-password';
+    req.body.confirmPassword = 'my-super-strong-password-1';
+
+    await postCreateNewPassword(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][1].validationFailed).toBe(true);
+  });
+
+  it('then the code is checked against the API', async () => {
+    await postCreateNewPassword(req, res);
+
+    expect(userCodesGetCode.mock.calls).toHaveLength(1);
+    expect(userCodesGetCode.mock.calls[0][0]).toBe(expectedUserCodeId);
+    expect(userCodesGetCode.mock.calls[0][1]).toBe(req.id);
+    expect(userCodesGetCode.mock.calls[0][2]).toBe('ConfirmMigratedEmail');
+  });
+
+  it('then if no data is returned from the code API then an error is returned', async () => {
+    userCodesGetCode.mockReset().mockReturnValue(null);
+
+    await postCreateNewPassword(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][1].validationFailed).toBe(true);
+  });
+
+  it('then the user is created if the request is valid', async () => {
+    await postCreateNewPassword(req, res);
+
+    expect(createUser.mock.calls).toHaveLength(1);
+    expect(createUser.mock.calls[0][0]).toBe(expectedEmail);
+    expect(createUser.mock.calls[0][1]).toBe(expectedPassword);
+    expect(createUser.mock.calls[0][2]).toBe('Roger');
+    expect(createUser.mock.calls[0][3]).toBe('Johnson');
+    expect(createUser.mock.calls[0][4]).toBe(req.id);
+  });
+
+  it('then if an error is returned while creating the user an error is returned', async () => {
+    createUser.mockReset().mockReturnValue(null);
+
+    await postCreateNewPassword(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][1].validationFailed).toBe(true);
+  });
+
+  it('then the user is mapped to the organisation', async () => {
+    await postCreateNewPassword(req, res);
+
+    expect(createOrg.mock.calls).toHaveLength(1);
+    expect(createOrg.mock.calls[0][0]).toBe(expectedUserId);
+    expect(createOrg.mock.calls[0][1]).toBe('svc1');
+    expect(createOrg.mock.calls[0][2]).toBe('MAT1234');
+    expect(createOrg.mock.calls[0][3]).toBe('010');
+    expect(createOrg.mock.calls[0][4]).toBe(req.id);
+  });
+
+  it('then if the user is successfully created the usercode is deleted', async () => {
+    await postCreateNewPassword(req, res);
+
+    expect(userCodesDeleteCode.mock.calls).toHaveLength(1);
+    expect(userCodesDeleteCode.mock.calls[0][0]).toBe(expectedUserCodeId);
+    expect(userCodesDeleteCode.mock.calls[0][1]).toBe(req.id);
+    expect(userCodesDeleteCode.mock.calls[0][2]).toBe('ConfirmMigratedEmail');
+  });
+
+  it('then the usercode is deleted if the user already exists', async () => {
+    createUser.mockReset().mockReturnValue(null);
+
+    await postCreateNewPassword(req, res);
+
+    expect(userCodesDeleteCode.mock.calls).toHaveLength(1);
+  });
+
+  it('then the user is redirected to the migration complete view when the request is valid', async () => {
+    await postCreateNewPassword(req, res);
+
+    expect(res.redirect.mock.calls).toHaveLength(1);
+    expect(res.redirect.mock.calls[0][0]).toBe(`/${req.params.uuid}/migration/complete`);
+  });
+});
