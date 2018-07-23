fc2ec5f9114f72044ea7e029858cc5eb64c3fd63
Simon Bull <simon.bull.84@googlemail.com>
2018-07-23 16:38:52 +0100

    Update flow to check existing user after code entry

--- test/migrationTests/postMigratedEmail.test.js
+++ test/migrationTests/postMigratedEmail.test.js
@@ -13,13 +13,11 @@ describe('When posting to confirm the migration email view', () => {
   let req;
   let res;
   let upsertCodeStub;
-  let findUserStub;
   const expectedViewToDisplay = 'no-email';
   const expectedEmail = 'test@tester.local';
   const expectedClientId = 'test-client-1';
   const expectedRedirectUri = 'https://local.test';
   const expectedUserCodeId = 'some-uid';
-  const expectedUserId = 'some-user-uid';
 
   beforeEach(() => {
     req = utils.mockRequest();
@@ -41,10 +39,6 @@ describe('When posting to confirm the migration email view', () => {
     upsertCodeStub = jest.fn().mockReturnValue({ code: '', uid: expectedUserCodeId });
     const userCodes = require('./../../src/infrastructure/UserCodes');
     userCodes.upsertCode = upsertCodeStub;
-
-    findUserStub = jest.fn().mockReset().mockReturnValue(null);
-    const users = require('./../../src/infrastructure/Users');
-    users.find = findUserStub;
   });
 
   it('then if the email is not valid an error is returned and the view from the body displayed', async () => {
@@ -87,24 +81,5 @@ describe('When posting to confirm the migration email view', () => {
 
     expect(res.redirect.mock.calls).toHaveLength(1);
     expect(res.redirect.mock.calls[0][0]).toBe(`/${req.params.uuid}/migration/${expectedUserCodeId}/confirm-email`);
-  });
-
-  it('then the email address is checked to see if it is already registered', async () => {
-    await getRequestPasswordReset(req, res);
-
-    expect(findUserStub.mock.calls).toHaveLength(1);
-    expect(findUserStub.mock.calls[0][0]).toBe(expectedEmail);
-    expect(findUserStub.mock.calls[0][1]).toBe(req.id);
-  });
-
-  it('then if the email address is in use the user is redirected to the email in use page', async () => {
-    findUserStub = jest.fn().mockReset().mockReturnValue({ sub: expectedUserId });
-    const users = require('./../../src/infrastructure/Users');
-    users.find = findUserStub;
-
-    await getRequestPasswordReset(req, res);
-
-    expect(res.redirect.mock.calls).toHaveLength(1);
-    expect(res.redirect.mock.calls[0][0]).toBe(`/${req.params.uuid}/migration/email-in-use`);
   });
 });
