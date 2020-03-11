77ab44caa3d86b3b1fbae80b2533564e094a3ac7
jovin-joy <jovin.joy@education.gov.uk>
2020-03-11 14:57:48 +0000

    NSA-2843

--- src/app/select-organisation/index.js
+++ src/app/select-organisation/index.js
@@ -8,8 +8,8 @@ const InteractionComplete = require('./../InteractionComplete');
 const appendInteractionDetails = require('./../utils/appendInteractionDetails');
 const { getServiceById } = require('./../../infrastructure/applications');
 const { getRolesOfService, listUserServices } = require('./../../infrastructure/access');
-// const config = require('./../../infrastructure/Config')();
 const router = express.Router({ mergeParams: true });
+const config = require('./../../infrastructure/Config')();
 
 const getNaturalIdentifiers = (orgsForUser) => {
   for (let i = 0; i < orgsForUser.length; i++) {
@@ -45,9 +45,10 @@ const getAction = async (req, res) => {
   }
 
   let orgsForUser = await organisationApi.associatedWithUserV2(uid);
-  //const coronaVirusFormRedirectUri = config.coronavirusform? config.coronavirusform.redirect : null;
-  logger.info( 'corona virus form redirect_uri =' + req.query.redirect_uri +' config.coronavirusform.redirect::');
-  if(req.query.redirect_uri !== 'https://signin-pp-aforms-as.azurewebsites.net/oidc/cb') {
+
+  const coronaVirusFormRedirectUri = config.coronaVirusForm ? config.coronaVirusForm.redirect : null;
+  logger.info('corona virus form redirect_uri =' + req.query.redirect_uri +' config.coronavirusform.redirect::' + coronaVirusFormRedirectUri);
+  if(req.query.redirect_uri !== coronaVirusFormRedirectUri) {
     const application = await getServiceById(req.interaction.client_id, req.id);
     if (application) {
       const serviceRoles = await getRolesOfService(application.id, req.id);
@@ -84,9 +85,10 @@ const postAction = async (req, res) => {
   const uid = req.query.uid;
   if (!req.body['selected-organisation']) {
     let orgsForUser = await organisationApi.associatedWithUserV2(uid);
-   // const coronaVirusFormRedirectUri = config.coronavirusform? config.coronavirusform.redirect : null;
-    logger.info( 'corona virus form redirect_uri =' + req.query.redirect_uri +' config.coronavirusform.redirect::');
-    if(req.query.redirect_uri !== 'https://signin-pp-aforms-as.azurewebsites.net/oidc/cb') {
+
+    const coronaVirusFormRedirectUri = config.coronaVirusForm? config.coronaVirusForm.redirect : null;
+    logger.info('corona virus form redirect_uri =' + req.query.redirect_uri +' config.coronaVirusForm.redirect::' + coronaVirusFormRedirectUri);
+    if(req.query.redirect_uri !== coronaVirusFormRedirectUri) {
       const application = await getServiceById(req.interaction.client_id, req.id);
       if (application) {
         const serviceRoles = await getRolesOfService(application.id, req.id);
