48fdae02e822596ae675607b0491607805383319
Steve Coleman-Williams <steve@archronos.com>
2020-05-01 08:32:49 +0100

    removed unused vars

--- b2c-app/src/App.js
+++ b2c-app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login';
 
 import {
-  Router,
   Switch,
-  Route,
-  NavLink
+  Route
 } from "react-router-dom";
 
 function App() {
