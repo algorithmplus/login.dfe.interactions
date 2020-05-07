4a3df8fb90121d1a7f3bc69d161d54a3218c2e90
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-05-07 17:28:56 +0100

    Removed password field in api element

--- b2c-app/src/components/B2C/B2C.js
+++ b2c-app/src/components/B2C/B2C.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function b2c() {
     return (
         <div id="api">
-            <input type="password" id="email" name="Email address" pattern="^[a-zA-Z0-9.!#$%&amp;’'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" placeholder="Email Address" value="" tabindex="1"></input>
+
         </div>
     )
 }
