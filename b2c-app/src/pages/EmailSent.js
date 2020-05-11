ce0ef41da67d3cd1e1d91e64eaa7500cdb84ea45
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-05-11 09:48:48 +0100

    Updated email sent page

--- b2c-app/src/pages/EmailSent.js
+++ b2c-app/src/pages/EmailSent.js
@@ -15,6 +15,9 @@ export default function EmailSent() {
                             <p className="govuk-body">Check your spam folder if you can't see it in a few minutes.</p>
                             {/* <p className="govuk-body">If you don't receive an email after this time you can resend the activation email.</p> */}
                             <p className="govuk-body">This link expires in 24 hours.</p>
+                            <p className="govuk-body">
+                                <a href="./signup" className="govuk-link">I entered the wrong email address</a>
+                            </p>
                         </div>
                     </div>
                 </main>
