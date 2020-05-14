601b97b3989a83eca326428bcc60e5134a43e294
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-05-14 12:57:14 +0100

    Putting api element back and removing custom text

--- b2c-app/src/pages/AccountActivated.js
+++ b2c-app/src/pages/AccountActivated.js
@@ -14,7 +14,7 @@ export default function AccountActivated() {
                     <div className="govuk-grid-row">
                         <div className="govuk-grid-column-two-thirds">
                             <components.PageTitle size='xl' title="We've activated your account"/>
-                            <p className="govuk-body">You can start using your account to access your information.</p>
+                            <components.B2C />
                             <a href={getB2CLink('login')} role="button" draggable="false" class="govuk-button govuk-button--start" data-module="govuk-button">
                                 Sign in to your account
                             </a>
