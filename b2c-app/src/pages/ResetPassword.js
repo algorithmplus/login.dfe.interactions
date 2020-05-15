a8c44edec157d5e8260b4031c1c7e3cfdbd87b2e
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-05-15 17:28:42 +0100

    Reset password page

--- b2c-app/src/pages/ResetPassword.js
+++ b2c-app/src/pages/ResetPassword.js
@@ -14,8 +14,11 @@ export default function ResetPassword() {
                     <div className="govuk-grid-row">
                         <div className="govuk-grid-column-two-thirds">
                             <components.PageTitle size='xl' title="Access your account"/>
+                            <p className="govuk-body">You can reset your password if you've forgotten it.</p>
+                            <p className="govuk-body">If you cannot remember your email address you can also retrieve it here.</p>
+                            <h3 class="govuk-heading-m">Reset your password</h3>
+                            <p className="govuk-body">To reset your password we need to send an email to the address registered to your account.</p>
                             <components.B2C />
-                            
                         </div>
                     </div>
                 </main>
