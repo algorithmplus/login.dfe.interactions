e3d053a5ed53741f94af39699a375c1e9168dede
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-05-15 11:09:04 +0100

    Moving forgot password outside its container

--- b2c-app/public/js-static/domManipulation.js
+++ b2c-app/public/js-static/domManipulation.js
@@ -164,6 +164,17 @@
                 });
             }
 
+            //manipulate the DOM to move forgot password link outside its parent
+            if(this.forgotPassword){
+                //get its actual wrapper that contains the class
+                this.forgotPasswordWrapper = this.forgotPassword.parentNode;
+                if (this.forgotPasswordWrapper){
+                    this.forgotPasswordContainer = this.forgotPasswordWrapper.parentNode;
+                    //move the password element with its class, put it after its current container
+                    this.forgotPasswordContainer.parentNode.insertBefore(this.forgotPasswordWrapper, this.forgotPasswordContainer.nextSibling);
+                }
+            }
+
 
             // manipulate the DOM so that we can include the password help item
             this._passWordHelp = document.createElement('div');
