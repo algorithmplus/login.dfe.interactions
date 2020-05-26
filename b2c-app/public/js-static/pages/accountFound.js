7de36fea16bf7eddc0208acc95cbc255f95ddb33
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-05-26 12:11:47 +0100

    Account found page

---
+++ b2c-app/public/js-static/pages/accountFound.js
@@ -0,0 +1,13 @@
+(function () {
+
+    var Controller = function Controller() {
+        
+        this.init = function init() {
+            document.title = 'We\'ve found your email address | National Careers Service';  
+        }
+
+    }
+
+    new Controller().init();
+
+})();
