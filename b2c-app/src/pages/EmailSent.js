d40cc21297ab168a59ab876e7be27627518633ea
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-05-27 11:36:54 +0100

    Added logic to reuse email sent page in two flows

--- b2c-app/src/pages/EmailSent.js
+++ b2c-app/src/pages/EmailSent.js
@@ -3,7 +3,7 @@ import components from '../components';
 import { getB2CLink } from '../helpers/urls';
 import { ACTIONS } from '../constants/actions';
 
-export default function EmailSent() {
+export default function EmailSent ( props ) {
     return (
         <div id="emailSent">
 
@@ -18,11 +18,15 @@ export default function EmailSent() {
                             <components.PageTitle size='xl' title="We've sent you an email"/>
                             <components.B2C />
                             <p className="govuk-body">Check your spam folder if you can't see it in a few minutes.</p>
-                            {/* <p className="govuk-body">If you don't receive an email after this time you can resend the activation email.</p> */}
+                            <components.ResendActivationLink action={props.action}/>
                             <p className="govuk-body">This link expires in 24 hours.</p>
-                            <p className="govuk-body">
-                                <a href={getB2CLink(ACTIONS.SIGNUP)} className="govuk-link">I entered the wrong email address</a>
-                            </p>
+
+                            {/* show link to signup again if entered wrong address (only signup flow) */}
+                            {props.action === ACTIONS.SIGNUP && (
+                                <p className="govuk-body">
+                                    <a href={getB2CLink(ACTIONS.SIGNUP)} className="govuk-link">I entered the wrong email address</a>
+                                </p>
+                            )}
                         </div>
                     </div>
                 </main>
