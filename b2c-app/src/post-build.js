c4caee8aa2209bb5f9f174435b91701dbb22e074
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-06-05 18:14:09 +0100

    Removed static JS files

--- b2c-app/src/post-build.js
+++ b2c-app/src/post-build.js
@@ -12,8 +12,8 @@ fs.readFile(indexFile, 'utf8', (err, data) => {
     }
 
     //let newFile = data.replace(/(\r\n)*<script.*?\/script>(\r\n)*/g, '');
-    let newFile = data.replace(/client-script/g, 'script');
-    newFile = newFile.replace(/rel="stylesheet"/g, 'rel="stylesheet" data-preload="true"');
+    // let newFile = data.replace(/client-script/g, 'script');
+    let newFile = data.replace(/rel="stylesheet"/g, 'rel="stylesheet" data-preload="true"');
 
     fs.writeFile(filename, newFile, (err) => {
         console.log(err);
